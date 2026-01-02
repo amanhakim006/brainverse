7e5db50f69593be370a6d20b3f029566c1f000d3
amanhakim006 <amanhakim777@gmail.com>
2026-01-02 22:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,42 +2,40 @@
 const collectionData = {
     'stranger-things-1': [
         { img: 'images/ST1.jpg', title: 'The Demogorgon' },
-        { img: 'images/ST2.jpg', title: 'Hawkins Bike Squad' },
-        { img: 'images/ST3.jpg', title: 'Eleven Powers' },
-        { img: 'images/ST4.jpg', title: 'Starcourt Mall' }
+        { img: 'images/ST2.jpg', title: 'Will Get Power' },
+        { img: 'images/ST3.jpg', title: 'MR.Whatsit' },
+        { img: 'images/ST4.jpg', title: 'Henry as Mr.Whatsit' }
     ],
     'it-derry': [
-        { img: 'images/it1.jpg', title: 'Pennywise Smile' },
-        { img: 'images/it2.jpg', title: 'The Red Balloon' },
-        { img: 'images/it3.jpg', title: 'The Losers Club' },
-        { img: 'images/it4.jpg', title: 'Georgie\'s Boat' }
+        { img: 'images/it1.jpg', title: 'Smoky Run' },
+        { img: 'images/it2.jpg', title: 'Missing Poster' },
+        { img: 'images/it3.jpg', title: 'Pennywise Trapped' },
+        { img: 'images/it4.jpg', title: 'Pennywise Smile' }
     ],
     'rehman-dakait': [
-        { img: 'images/Rehman1.jpg', title: 'Rehman The King' },
-        { img: 'images/Rehman2.jpg', title: 'Ravine Ambush' },
-        { img: 'images/Rehman3.jpg', title: 'Campfire Tales' }
+        { img: 'images/Rehman1.jpg', title: 'Rehman Dakait' },
+        { img: 'images/Rehman2.jpg', title: 'King of Layari' },
     ],
     'stranger-things-new': [
-        { img: 'images/SThings1.jpg', title: 'Vecna\'s Lair' },
-        { img: 'images/SThings2.jpg', title: 'Russian Base' },
-        { img: 'images/SThings3.jpg', title: 'Eddie\'s Solo' }
+        { img: 'images/SThings1.jpg', title: 'Upsidedown Mumbai'},
+        { img: 'images/SThings2.jpg', title: 'Upsidedown Tajmahal' },
+        { img: 'images/SThings3.jpg', title: 'Upsidedown CST Mumbai' }
     ],
     'game-of-thrones': [
-        { img: 'images/GOT1.jpg', title: 'Dragon Fire' },
-        { img: 'images/GOT2.jpg', title: 'The Iron Throne' },
-        { img: 'images/GOT3.jpg', title: 'Night Watch' }
+        { img: 'images/GOT1.jpg', title: 'Dragon Fire Gatway of India' },
+        { img: 'images/GOT2.jpg', title: 'Dragon Fire Taj Mahal' },
     ],
     'hamza-dakait': [
-        { img: 'images/Hamza1.jpg', title: 'Hamza The Warrior' },
-        { img: 'images/Hamza2.jpg', title: 'Desert Chase' },
+        { img: 'images/Hamza1.jpg', title: 'Hamza Splashes' },
+        { img: 'images/Hamza2.jpg', title: 'Hamza Smokes' },
         { img: 'images/Hamza3.jpg', title: 'The Hideout' },
-        { img: 'images/Hamza4.jpg', title: 'Final Stand' }
+        { img: 'images/Hamza4.jpg', title: 'Hamza Rides' }
     ],
     'winter-vibes': [
-        { img: 'images/snow1.jpg', title: 'Cozy Cabin' },
-        { img: 'images/snow2.jpg', title: 'Frozen Lake' },
+        { img: 'images/snow1.jpg', title: 'Ice Skating' },
+        { img: 'images/snow2.jpg', title: 'Ice Ball ' },
         { img: 'images/snow3.jpg', title: 'Winter Portrait' },
-        { img: 'images/snow4.jpg', title: 'Northern Lights' }
+        { img: 'images/snow4.jpg', title: 'Walk into Snow' }
     ]
 };
 
